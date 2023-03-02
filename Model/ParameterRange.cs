99cc53deb65507e500821ca969b15350546c116c
eulerscheZahl <eulerschezahl@yandex.com>
2023-03-02 09:01:52 +0100

    new ParamRange notation

--- Model/ParameterRange.cs
+++ Model/ParameterRange.cs
@@ -18,15 +18,42 @@ namespace LocalEnv.Model
             this.MaxValue = range.MaxValue;
         }
 
-        public static ParameterRange Parse(string text)
+        public static IEnumerable<ParameterRange> Parse(string text)
         {
+            int count = 1;
+            if (text.Contains(":"))
+            {
+                count = int.Parse(text.Split(':')[1]);
+                text = text.Substring(0, text.IndexOf(':'));
+            }
             string[] parts = text.Split('-');
             ParameterRange range = new ParameterRange
             {
                 MinValue = double.Parse(parts[0]),
                 MaxValue = double.Parse(parts[^1]),
             };
-            return range;
+            if (count == 1)
+            {
+                yield return range;
+                yield break;
+            }
+            bool isInteger = int.TryParse(parts[0], out _) && int.TryParse(parts[^1], out _);
+            for (int c = count; c > 0; c--)
+            {
+                if (isInteger)
+                {
+                    ParameterRange r = new ParameterRange { MinValue = range.MinValue, MaxValue = (int)(range.MinValue - 1 + (range.MaxValue + 1 - range.MinValue) / c) };
+                    yield return r;
+                    range.MinValue = r.MaxValue + 1;
+                }
+                else
+                {
+                    ParameterRange r = new ParameterRange { MinValue = range.MinValue, MaxValue = Math.Round(range.MinValue + (range.MaxValue - range.MinValue) / c, 3) };
+                    yield return r;
+                    range.MinValue = r.MaxValue;
+                }
+            }
+            yield break;
         }
 
         public bool IsMatch(ParameterValue value) => value.Value >= MinValue && value.Value <= MaxValue;
